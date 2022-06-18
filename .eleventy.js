fa9a912291969cca2e093cbcd1d24b65ad4ee927
Romain Koenig <romain.koenig@gmail.com>
2022-06-18 17:00:24 +0200

    Looks more fun with a background

--- .eleventy.js
+++ .eleventy.js
@@ -6,13 +6,13 @@ module.exports = function (eleventyConfig) {
 
 	eleventyConfig.addPlugin(eleventyNavigationPlugin);
 
-	// eleventyConfig.addPassthroughCopy("./src/css/");
-	// eleventyConfig.addWatchTarget("./src/css/");
+	eleventyConfig.addPassthroughCopy("./src/css/");
+	eleventyConfig.addWatchTarget("./src/css/");
 
 	// eleventyConfig.addPassthroughCopy("./src/js/");
 	// eleventyConfig.addWatchTarget("./src/js/");
 
-	// eleventyConfig.addPassthroughCopy("src/pics");
+	eleventyConfig.addPassthroughCopy("src/pics");
 	eleventyConfig.addPassthroughCopy("src/favicon");
 
 	// Find and copy any `png` files, maintaining directory structure.
